23102c1aa6e47b3a900d155fe0a4ab1c53653d4b
lukecolpo <lwcolpo@gmail.com>
2021-11-27 14:23:28 -0500

    initial project commit 2

--- projects/KotlinJournal/src/main/java/Entry.kt
+++ projects/KotlinJournal/src/main/java/Entry.kt
@@ -1,2 +1,7 @@
-class Entry {
+import java.time.LocalDateTime
+
+class Entry(var dateTime: LocalDateTime, var text: String) {
+    override fun toString(): String{
+        return "$dateTime $text"
+    }
 }
